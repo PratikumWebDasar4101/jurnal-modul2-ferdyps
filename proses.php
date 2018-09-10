b5d037715d8be4cf2840af2c88c0f01c0f363573
ferdyps <42725335+ferdyps@users.noreply.github.com>
2018-09-10 10:02:15 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,22 @@
 <?php 
 //Tuliskan kode anda di bawah ini
+	$a = $_GET['a'];
 
+	for ($i=$a; $i >=1 ; $i--) { 
+		for ($j=$i; $j>= 1 ; $j--) { 
+			echo "$j";
+		}echo "<br>";
+	}
+
+	$genap= array();
+	$ganjil= array();
+	for ($i=1; $i <= $a ; $i++) { 
+		if ($i % 2 == 0) {
+			array_push($genap, $i);
+			print_r(array_push($genap, $i));
+		}else{
+			array_push($ganjil, $i);
+			print_r(array_push($ganjil, $i));
+		}
+	}
 ?>
